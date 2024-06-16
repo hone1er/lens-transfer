84dbf1498eb9874a4aff80a9e23ce2f83baf4451
hone1er <24376928+hone1er@users.noreply.github.com>
2024-06-16 12:39:41 +0200

    Manage follower list

--- src/components/composed/FollowSettingsManager.tsx
+++ src/components/composed/FollowSettingsManager.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   type FollowPolicy,
   FollowPolicyType,
@@ -10,8 +10,12 @@ import {
   Amount,
   erc20,
   ChainType,
+  useProfileFollowers,
+  type ProfileId,
+  type Profile,
+  TriStateValue,
+  useFollow,
 } from "@lens-protocol/react-web";
-import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
@@ -27,6 +31,8 @@ import {
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const FormSchema = z.object({
   followPolicy: z.enum(
@@ -46,10 +52,18 @@ export function FollowSettingsManager() {
   const { execute, loading, error } = useUpdateFollowPolicy();
   const { data: session, loading: loadingSession } = useSession();
 
+  const { data: followers } = useProfileFollowers({
+    of:
+      session?.authenticated && session.type === SessionType.WithProfile
+        ? session?.profile?.id
+        : ("" as ProfileId),
+  });
+
   const [selectedPolicy, setSelectedPolicy] = useState(FollowPolicyType.ANYONE);
   const [recipient, setRecipient] = useState("");
   const [amount, setAmount] = useState("");
 
+  console.log("🚀 ~ FollowSettingsManager ~ followers:", followers);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
@@ -89,13 +103,13 @@ export function FollowSettingsManager() {
     resolveFollowPolicy(session?.profile);
 
   return (
-    <div className="container relative mx-auto px-4 py-12 md:px-6 md:py-16 ">
+    <div className="container relative mx-auto  px-4 py-12 md:px-6 md:py-16 ">
       {/* coming soon layover */}
       {/* <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center rounded-xl bg-gray-900 bg-opacity-50 font-bold text-white">
         <p>Follow Policy Manger is coming soon!</p>
       </div> */}
-      <div className="z-0 mx-auto max-w-2xl ">
-        <div className="rounded-lg bg-white p-6 shadow-lg  dark:bg-gray-900 md:p-8">
+      <div className="z-0 mx-auto flex max-w-2xl flex-col gap-12 ">
+        <div className=" rounded-lg bg-white p-6 shadow-lg  dark:bg-gray-900 md:p-8">
           <div className="mb-6">
             <h1 className="mb-2 text-2xl font-bold">Manage Follow Policy</h1>
             <p className="text-gray-500 dark:text-gray-400">
@@ -227,7 +241,7 @@ export function FollowSettingsManager() {
                     )}
                   />
                   <div className="flex justify-end">
-                    <Button type="submit" disabled={loading}>
+                    <Button size={"sm"} type="submit" disabled={loading}>
                       {loading ? "Saving..." : "Save Changes"}
                     </Button>
                   </div>
@@ -241,6 +255,61 @@ export function FollowSettingsManager() {
             )}
           </div>
         </div>
+        <FollowersList followers={followers} />
+      </div>
+    </div>
+  );
+}
+
+function FollowersList({ followers }: { followers: Profile[] | undefined }) {
+  const { execute, loading, error } = useFollow();
+
+  const handleFollow = async (follower: Profile) => {
+    await execute({ profile: follower });
+  };
+  if (!followers) {
+    return null;
+  }
+  return (
+    <div className="space-y-4">
+      <h2 className="mb-2 text-lg font-semibold">Followers</h2>
+      <div className="space-y-4">
+        {followers.map((follower) => {
+          const imgSrc =
+            follower.metadata?.picture?.__typename === "ImageSet"
+              ? follower.metadata.picture?.optimized?.uri
+              : follower.metadata?.picture?.image.optimized?.uri;
+
+          const canFollow = follower.operations.canFollow === TriStateValue.Yes;
+          return (
+            <div key={follower.id} className="flex items-center space-x-4">
+              <div className="relative flex w-full items-center space-x-2">
+                <Avatar className="h-16 w-16">
+                  <AvatarImage src={imgSrc} />
+                  <AvatarFallback>H1</AvatarFallback>
+                </Avatar>
+                <p>
+                  {follower.handle?.suggestedFormatted.localName ??
+                    follower?.handle?.localName ??
+                    ""}
+                </p>
+              </div>
+              {canFollow ? (
+                <Button
+                  size={"sm"}
+                  variant={"outline"}
+                  disabled={loading}
+                  onClick={() => handleFollow(follower)}
+                >
+                  Follow
+                </Button>
+              ) : null}
+              <Button size={"sm"} variant={"destructive"}>
+                Remove
+              </Button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
